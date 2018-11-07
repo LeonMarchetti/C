bb335b6b01e4fe669f989a14732a424c285d0102
LeonMarchetti <mleonardoa@gmail.com>
2018-11-07 19:34:55 -0300

    Nueva función para obtener valores booleanos

--- Servidor-Switch/json-util.c
+++ Servidor-Switch/json-util.c
@@ -15,3 +15,10 @@ const int json_get_int(json_object* objeto, char* clave)
     json_object_object_get_ex(objeto, clave, &objeto_hijo);
     return json_object_get_int(objeto_hijo);
 }
+
+const int json_get_bool(json_object* objeto, char* clave)
+{
+    json_object* objeto_hijo;
+    json_object_object_get_ex(objeto, clave, &objeto_hijo);
+    return json_object_get_boolean(objeto_hijo);
+}
